75940c0f59288bb5c3c7ca9b0d555dee2044944b
ph-swadeb-07 <swadebsharma2022@gmail.com>
2023-02-02 02:02:42 +0600

    added reverse

---
+++ reverse.js
@@ -0,0 +1,12 @@
+// 
+
+function reverseString(text){
+    for( let i = 0; i <text.length; i++){
+        const element = text[1];
+        console.log(element);
+    }
+}
+
+const myString = 'I am a good boy';
+const reverse = reverseString(myString);
+
